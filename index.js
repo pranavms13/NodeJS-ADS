41b9e268be21ef7ad168f828962a09a6c0c79b4b
Pranav M S <pranavms13@yahoo.com>
2019-12-10 22:36:14 +0530

    Security

--- index.js
+++ index.js
@@ -2,15 +2,27 @@
 const express = require('express');
 const app = express();
 const shell = require('shelljs');
+//const crypto = require('crypto');
 
 exports.start = function (){
+    var hmac;
     app.listen(1025, () => { 
         console.log('Update Port : ' + 1025);
     });
     
     app.post('/deployment/updatetheapp', (req, res) => {
-        var cmd = "echo ========================================================;echo ------------------- Updater Script ---------------------;echo ---------------- Connecting to GitHub ------------------;git pull;echo -------------- Updating Node modules -------------------;npm update;npm install;echo ------------------Update completed----------------------;exit"
-        shell.exec(cmd)
+        var rhash = req.headers['X-Hub-Signature'];
+        var body = req.body;
+        hmac = crypto.createHmac('sha1','pranav')
+        hmac.update(JSON.stringify(body)).digest('hex');
+        if(rhash=='sha1='+hmac){
+            console.log("verified");
+        }
+        else{
+            console.log("Not Verified");
+        }
+        //var cmd = "echo ========================================================;echo ------------------- Updater Script ---------------------;echo ---------------- Connecting to GitHub ------------------;git pull;echo -------------- Updating Node modules -------------------;npm update;npm install;echo ------------------Update completed----------------------;exit"
+        //shell.exec(cmd)
         res.send({"message":"Update in progress"})
     });
 }
