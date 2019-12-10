62f301390a7f6b2b1cce586330f51a0b8687d3ef
Pranav M S <pranavms13@yahoo.com>
2019-12-10 23:04:44 +0530

    hmac test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const shell = require('shelljs');
+var payload = require('request-payload');
 //const crypto = require('crypto');
 
 exports.start = function (){
@@ -12,15 +13,17 @@ exports.start = function (){
     
     app.post('/deployment/updatetheapp', (req, res) => {
         var rhash = req.headers['X-Hub-Signature'];
-        var body = req.body;
-        hmac = crypto.createHmac('sha1','pranav')
-        hmac.update(JSON.stringify(body)).digest('hex');
-        if(rhash=='sha1='+hmac){
-            console.log("verified");
-        }
-        else{
-            console.log("Not Verified");
-        }
+        payload(req, function(body) {
+            hmac = crypto.createHmac('sha1','pranav')
+            hmac.update(JSON.stringify(body)).digest('hex');
+            if(rhash=='sha1='+hmac){
+                console.log("verified");
+            }
+            else{
+                console.log("Not Verified");
+            }
+        });
+        
         //var cmd = "echo ========================================================;echo ------------------- Updater Script ---------------------;echo ---------------- Connecting to GitHub ------------------;git pull;echo -------------- Updating Node modules -------------------;npm update;npm install;echo ------------------Update completed----------------------;exit"
         //shell.exec(cmd)
         res.send({"message":"Update in progress"})
