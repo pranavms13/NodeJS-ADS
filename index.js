9c0395cf9b39f96f74b00b93d410579d679137c0
Pranav M S <pranavms13@yahoo.com>
2019-12-10 19:33:29 +0530

    Fix

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 const shell = require('shelljs');
 
-app.listen(1025, () => { 
-    console.log('Update Port : ' + 1025);
-});
-
-app.get('/deployment/updatetheapp', (req, res) => {
-    shell.exec('./deploy.sh')
-});
+function start(){
+    app.listen(1025, () => { 
+        console.log('Update Port : ' + 1025);
+    });
+    
+    app.get('/deployment/updatetheapp', (req, res) => {
+        shell.exec('./deploy.sh')
+    });
+}
