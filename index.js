66daddf84fc9e0f1ed37a00f798e5697c94b26a3
Pranav M S <pranavms13@yahoo.com>
2019-12-10 20:23:38 +0530

    small fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ exports.start = function (){
         console.log('Update Port : ' + 1025);
     });
     
-    app.get('/deployment/updatetheapp', (req, res) => {
+    app.post('/deployment/updatetheapp', (req, res) => {
         shell.exec('./deploy.sh')
         res.send({"message":"Update in progress"})
     });
